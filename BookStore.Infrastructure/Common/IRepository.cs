9cac2bd31ee38584f2a3c1012038e220c9f193a4
Drexysw <knevdani@gmail.com>
2024-04-11 08:31:24 +0300

    Crated repository patter-unit of work

---
+++ BookStore.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace BookStore.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+        Task SaveChangesAsync();
+
+        Task<T> GetByIdAsync<T>(object id) where T : class;
+
+        Task<T> GetByIdsAsync<T>(object[] id) where T : class;
+
+        Task DeleteAsync<T>(object id) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        void Detach<T>(T entity) where T : class;
+    }
+}
---
+++ BookStore.Infrastructure/Common/Repository.cs
@@ -0,0 +1,63 @@
+﻿using BookStore.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Infrastructure.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly ApplicationDbContext context;
+        public Repository(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return this.context.Set<T>();
+        }
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public Task<T> GetByIdAsync<T>(object id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdsAsync<T>(object[] id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync<T>(object id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Detach<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
