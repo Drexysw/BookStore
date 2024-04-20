c18b0a68e28da2717d958c2826703f4c7ab9a17c
Drexysw <knevdani@gmail.com>
2024-04-20 12:01:57 +0300

    admin antiforgery token

--- BookStore/Areas/Admin/Controllers/AdminBaseController.cs
+++ BookStore/Areas/Admin/Controllers/AdminBaseController.cs
@@ -5,6 +5,7 @@ namespace BookStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize(Roles = AdminRole)]
+    [AutoValidateAntiforgeryToken]
     public class AdminBaseController : Controller
     {
         
